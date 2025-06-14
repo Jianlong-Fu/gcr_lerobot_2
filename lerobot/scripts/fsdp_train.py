24dc61dc2ac461debd50c12308cb119f56a4e4e6
xjh0803 <1259007219@qq.com>
2025-06-14 22:29:25 +0800

    Optimi data resume method

--- lerobot/scripts/fsdp_train.py
+++ lerobot/scripts/fsdp_train.py
@@ -16,6 +16,7 @@
 import logging
 import time
 import os
+import math
 import glob
 import json
 import functools
@@ -30,6 +31,7 @@ from contextlib import nullcontext
 
 import torch
 import torch.distributed as dist
+from torch.utils.data import Subset
 from torch.distributed.fsdp import (
     FullyShardedDataParallel as FSDP,
     MixedPrecision,
@@ -416,7 +418,6 @@ def train(cfg: TrainPipelineConfig):
         logger.info(pformat(cfg.to_dict()))
     
     model.train()
-    dataloader_iter = cycle(dataloader)
     
     fwd_bwd_time = 0.0
     dataloading_s = 0.0
@@ -429,8 +430,32 @@ def train(cfg: TrainPipelineConfig):
         for _ in range(int((step-1)/cfg.gradient_accumulation_steps)):
             lr_scheduler.step()
         logger.info("Resuming Data Batch")
-        for _ in tqdm(range(int(step)), desc="Resuming Data Batch"):
-            next(dataloader_iter)
+        sampler.set_epoch(0)
+        all_indices = list(sampler)
+        samples_per_epoch = len(all_indices)
+        batches_per_epoch = math.ceil(samples_per_epoch / cfg.batch_size)
+        epoch_num = step // batches_per_epoch
+        batch_in_epoch = step % batches_per_epoch
+        sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank,
+                             shuffle=True, seed=seed)
+        sampler.set_epoch(epoch_num)
+        epoch_indices = list(sampler)
+        
+        start_idx = batch_in_epoch * cfg.batch_size
+        resume_indices = epoch_indices[start_idx : ]
+        
+        resume_dataset = Subset(dataset, resume_indices)
+        
+        dataloader = DataLoader(
+            resume_dataset,
+            batch_size=cfg.batch_size,
+            shuffle=False,         # 已经在 Subset 里做过 shuffle
+            num_workers=3,
+            collate_fn=extra_collate_fn,
+            pin_memory=False,
+        )
+        
+    dataloader_iter = cycle(dataloader)
     
     if rank == 0:
         logger.info("Starting training loop...")
