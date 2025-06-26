dbeec3449f4a86ff194ada291c842dbf2108f988
xjh0803 <1259007219@qq.com>
2025-06-26 18:34:33 +0800

    Add Norm to Pizza Dataset

--- lerobot/scripts/fsdp_train.py
+++ lerobot/scripts/fsdp_train.py
@@ -386,7 +386,7 @@ def train(cfg: TrainPipelineConfig):
         dataset,
         batch_size=cfg.batch_size,
         sampler=sampler,
-        num_workers=3,
+        num_workers=8,
         collate_fn=extra_collate_fn,
         pin_memory=False,
     )
